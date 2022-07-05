ae527d5112e39cd9d46e6267c709f618da9633e2
ndimoforaretas <ndimaret@gmail.com>
2022-07-05 06:39:07 +0200

    custom hooks created

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
-class App extends React.Component {
-	state = { videos: [], selectedVideo: null };
+const App = () => {
+	const [selectedVideo, setSelectedVideo] = useState(null);
+	const [videos, search] = useVideos("quotes about time");
 
-	componentDidMount() {
-		this.onTermSubmit("quote about time");
-	}
+	useEffect(() => {
+		setSelectedVideo(videos[0]);
+	}, [videos]);
 
-	onTermSubmit = async (term) => {
-		const response = await youtube.get("/search", {
-			params: {
-				q: term,
-			},
-		});
+	return (
+		<div className="ui container">
+			<SearchBar onFormSubmit={search} />
 
-		this.setState({
-			videos: response.data.items,
-			selectedVideo: response.data.items[0],
-		});
-	};
-
-	onVideoSelect = (video) => {
-		this.setState({ selectedVideo: video });
-	};
-
-	render() {
-		return (
-			<div className="ui container">
-				<SearchBar onFormSubmit={this.onTermSubmit} />
-
-				<div className="ui grid">
-					<div className="ui row">
-						<div className="eleven wide column">
-							<VideoDetail video={this.state.selectedVideo} />
-						</div>
-						<div className="five wide column">
-							<VideoList
-								onVideoSelect={this.onVideoSelect}
-								videos={this.state.videos}
-							/>
-						</div>
+			<div className="ui grid">
+				<div className="ui row">
+					<div className="eleven wide column">
+						<VideoDetail video={selectedVideo} />
+					</div>
+					<div className="five wide column">
+						<VideoList onVideoSelect={setSelectedVideo} videos={videos} />
 					</div>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default App;
